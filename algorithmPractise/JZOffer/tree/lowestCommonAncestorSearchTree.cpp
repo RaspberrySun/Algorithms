7bb3d18571664800ecd70decdfe2f7c2afdb5cbc
sunbo <sunbo@cmss.chinamobile.com>
2021-04-28 01:24:13 +0800

    剑指 Offer 68 - I. 二叉搜索树的最近公共祖先

---
+++ algorithmPractise/JZOffer/tree/lowestCommonAncestorSearchTree.cpp
@@ -0,0 +1,28 @@
+//
+// Created by hp on 2021/4/28.
+//
+
+
+/**
+ * @author RaspberrySun@outlook.com
+ * @Date 2021/4/20 10:21
+ */
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left,p,q);
+        if(root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        return root;
+    }
+};
